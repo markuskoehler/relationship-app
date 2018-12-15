8df6480b34423ee70a6bee900e71dfe484f6dd72
Markus Koehler <info@koehler.pro>
2018-12-15 19:18:53 +0100

    include and register open-iconic icons

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import BootstrapVue from 'bootstrap-vue'
 import '@ionic/pwa-elements'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/open-iconic/font/css/open-iconic-bootstrap.min.css'
 
 Vue.config.productionTip = false
 
